6c42df1d7c116b824b2b8209aef70c467cb2e0d9
AlexanderMaizanov <alexander.maizanov@gmail.com>
2025-05-16 16:04:40 +0300

    Nuget updates

--- Source/GlobalAssemblyInfo.cs
+++ Source/GlobalAssemblyInfo.cs
@@ -2,9 +2,9 @@ using System.Reflection;
 
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyCopyright("Copyright © 2024 Alexander Maizanov")]
+[assembly: AssemblyCopyright("Copyright © 2025 Alexander Maizanov")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("2024.9.12.0")]
-[assembly: AssemblyFileVersion("2024.9.12.0")]
+[assembly: AssemblyVersion("2025.1.1.0")]
+[assembly: AssemblyFileVersion("2025.3.12.0")]
